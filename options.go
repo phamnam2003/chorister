7b85942638b374ed0d96cdd69239626a13ac3a8d
phamnam2003 <namphamhai7@gmail.com>
2026-01-04 17:46:01 +0700

    feat(options): inherit options of ants

---
+++ options.go
@@ -0,0 +1,74 @@
+package chorister
+
+import (
+	"time"
+
+	"github.com/phamnam2003/ants/v2"
+)
+
+// Option represents the optional function.
+type Option func(opts *Options)
+
+// Options represents configuration options for the chorister package.
+type Options struct {
+	// Options from the ants package.
+	// It serves as the base configuration for goroutine pool management.
+	// Includes settings like logger, expiry duration, panic handler, non-blocking, etc.
+	ants.Options
+}
+
+// WithOptions accepts the whole Options config.
+func WithOptions(options Options) Option {
+	return func(opts *Options) {
+		*opts = options
+	}
+}
+
+// WithExpiryDuration sets up the interval time of cleaning up goroutines.
+func WithExpiryDuration(expiryDuration time.Duration) Option {
+	return func(opts *Options) {
+		opts.ExpiryDuration = expiryDuration
+	}
+}
+
+// WithPreAlloc indicates whether it should malloc for workers.
+func WithPreAlloc(preAlloc bool) Option {
+	return func(opts *Options) {
+		opts.PreAlloc = preAlloc
+	}
+}
+
+// WithMaxBlockingTasks sets up the maximum number of goroutines that are blocked when it reaches the capacity of pool.
+func WithMaxBlockingTasks(maxBlockingTasks int) Option {
+	return func(opts *Options) {
+		opts.MaxBlockingTasks = maxBlockingTasks
+	}
+}
+
+// WithNonblocking indicates that pool will return nil when there is no available workers.
+func WithNonblocking(nonblocking bool) Option {
+	return func(opts *Options) {
+		opts.Nonblocking = nonblocking
+	}
+}
+
+// WithPanicHandler sets up panic handler.
+func WithPanicHandler(panicHandler func(any)) Option {
+	return func(opts *Options) {
+		opts.PanicHandler = panicHandler
+	}
+}
+
+// WithLogger sets up a customized logger.
+func WithLogger(logger ants.Logger) Option {
+	return func(opts *Options) {
+		opts.Logger = logger
+	}
+}
+
+// WithDisablePurge indicates whether we turn off automatically purge.
+func WithDisablePurge(disable bool) Option {
+	return func(opts *Options) {
+		opts.DisablePurge = disable
+	}
+}
