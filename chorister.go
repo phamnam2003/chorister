213d374d2b085a3542a27788120bd54b1049fc65
phamnam2003 <namphamhai7@gmail.com>
2026-01-09 19:14:04 +0700

    feat(pool): create pool with ants

--- chorister.go
+++ chorister.go
@@ -3,6 +3,7 @@ package chorister
 import (
 	"errors"
 
+	"github.com/phamnam2003/ants/v2"
 	"github.com/phamnam2003/chorister/pkg/generic"
 )
 
@@ -20,10 +21,16 @@ type Chorister struct {
 }
 
 // NewChorister creates a new Chorister instance with the given pool size and options.
-func NewChorister(opts ...generic.Option[Options]) (*Chorister, error) {
+func NewChorister(poolSize int, opts ...generic.Option[Options]) (*Chorister, error) {
 	options := generic.LoadGenericOptions(opts...)
 
+	pool, err := ants.NewPool(poolSize, ants.WithOptions(options.Options))
+	if err != nil {
+		return nil, err
+	}
+
 	c := &Chorister{
+		CPool:   pool,
 		options: options,
 	}
 
